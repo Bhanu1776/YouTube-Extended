6409f7f77d5b0d17df4edc4913180e5d3ab40212
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-08-02 23:38:51 +0530

    feat: Added Navbar and Sidebar to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import './globals.css';
+import 'styles/globals.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import classNames from 'classnames';
 import { Analytics } from '@vercel/analytics/react';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import Navbar from '@components/Navigation/Navbar';
+import Sidebar from './components/Navigation/Sidebar';
 
 export const metadata = {
-  title: 'NextJs 13',
-  description: 'Nextjs 13 starter template',
+  title: 'Youtube',
+  description: 'The finest alternative to YouTube with additional features',
 };
 
 export default function RootLayout({
@@ -18,25 +18,99 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta property="og:title" content="Youtube Extended" />
+        <meta name="twitter:title" content="Youtube Extended" />
+
+        <meta name="description" content="Extended version of Youtube" />
+        <meta property="og:description" content="Extended version of Youtube" />
+        <meta
+          name="twitter:description"
+          content="Extended version of Youtube"
+        />
+
+        <meta property="og:image" content="https://i.imgur.com/AV7sLqE.png" />
+        <meta name="twitter:image" content="https://i.imgur.com/AV7sLqE.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="../public/favicons/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="../public/favicons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="../public/favicons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="../public/favicons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="../public/favicons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="../public/favicons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="../public/favicons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="../public/favicons/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="../public/favicons/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="../public/favicons/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../public/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="../public/favicons/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../public/favicons/favicon-16x16.png"
+        />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#e0c8fd" />
-
-        <meta property="og:title" content="Nextjs App" />
-        <meta name="twitter:title" content="Nextjs App" />
-
-        <meta name="description" content="Nextjs 13 starter template" />
-        <meta property="og:description" content="Nextjs 13 starter template" />
-        <meta name="twitter:description" content="Nextjs 13 starter template" />
-
-        <meta property="og:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
-        <meta name="twitter:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body
         className={classNames({
           'debug-screens': process.env.NODE_ENV === 'development',
         })}
       >
+        <Navbar />
+        <Sidebar />
         {children}
         <Analytics />
       </body>
