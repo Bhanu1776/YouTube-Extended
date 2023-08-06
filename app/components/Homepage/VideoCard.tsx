7e32ee27f5cb64e1fd8b1b58918b111d1389a99c
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-08-07 00:01:38 +0530

    chore(Home): Initialized Video component for the home page

---
+++ app/components/Homepage/VideoCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoCard = () => (
+  // const isMenuOpen = useSelector((store: RootState) => store.app.isMenuOpen);
+
+  <div>Bhanu</div>
+);
+export default VideoCard;
---
+++ app/components/Homepage/VideoContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import VideoCard from './VideoCard';
+
+const VideoContainer = () => (
+  <div>
+    <VideoCard />
+  </div>
+);
+
+export default VideoContainer;
