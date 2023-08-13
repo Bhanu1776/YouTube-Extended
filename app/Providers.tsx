5788bd3f85088ef9925651d850d29becff9a0f08
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-08-13 16:27:48 +0530

    feat(Home): Tanstack React-Query Implementation

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,8 +1,20 @@
 'use client';
 
 import { Provider } from 'react-redux';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+import { useState } from 'react';
 import store from '@store/store';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  const [client] = useState(new QueryClient());
+  return (
+    <QueryClientProvider client={client}>
+      <ReactQueryStreamedHydration>
+        <Provider store={store}>{children}</Provider>
+      </ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  );
 }
--- app/components/Homepage/VideoContainer.tsx
+++ app/components/Homepage/VideoContainer.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import Link from 'next/link';
+import { YOUTUBE_VIDEOS_API } from '@/utils/constants';
+import { Item } from '@/Types/HomepageTypes';
+import Spinner from '@/loading';
 import VideoCard from './VideoCard';
 
-const VideoContainer = () => (
-  <div>
-    <VideoCard />
-  </div>
-);
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  //* Using TanStack React Query to Fetch data
+  const { isLoading, isError, error } = useQuery({
+    // useQuery will take two args (name, callback func where we are supposed to fetch data) and if we want to increase cache time we can add one more arg with object and increase cacheTime: 500000
+    queryKey: ['HomePage Videos'],
+    queryFn: () => fetch(YOUTUBE_VIDEOS_API).then((res) => res.json()), //* Abstracted Business logic(API)
+
+    //* This will run only when data is fetched
+    onSuccess: (videoData) => {
+      setVideos(videoData.items);
+    },
+  });
+
+  if (isLoading) return <Spinner />;
+
+  if (isError) return `Error: ${(error as Error).message}`;
+
+  return (
+    <div className="ml-10 flex flex-wrap gap-4">
+      {videos.map((item: Item) => (
+        <Link href={`/watch?v=${item.id}`}>
+          <VideoCard key={item.id} info={item} />
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default VideoContainer;
