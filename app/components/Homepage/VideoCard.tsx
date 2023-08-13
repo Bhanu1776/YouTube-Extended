6ab6da05f9e4d59229846307d5c9f4395c67229b
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-08-13 16:39:20 +0530

    style: Initial Implementation of VideoCard for the Homepage

--- app/components/Homepage/VideoCard.tsx
+++ app/components/Homepage/VideoCard.tsx
@@ -1,8 +1,59 @@
 import React from 'react';
+import Link from 'next/link';
+import { Item } from '@Types/HomepageTypes';
 
-const VideoCard = () => (
-  // const isMenuOpen = useSelector((store: RootState) => store.app.isMenuOpen);
+const VideoCard = ({ info }: { info: Item }) => {
+  // ? Destructuring the api values
+  const {
+    contentDetails: { duration } = {},
+    snippet: {
+      thumbnails: { medium: { url } = {} },
+      title,
+      channelTitle,
+    },
+    statistics: { viewCount } = {},
+  } = info || undefined;
 
-  <div>Bhanu</div>
-);
+  return (
+    <div className="w-70 mb-3 flex h-60 flex-col gap-3 rounded-2xl p-0">
+      <div className="relative rounded-2xl">
+        <span className="absolute bottom-3 right-3 z-10 bg-gray-900 px-2 py-0.5 text-sm">
+          {duration}
+        </span>
+        <img
+          src={url}
+          className="w-90 h-48 select-none rounded-2xl"
+          alt="thumbnail"
+        />
+      </div>
+      <div className="flex gap-2">
+        <div className="min-w-fit">
+          <Link href="#">
+            <img src={url} alt="channel" className="h-9 w-9 rounded-full" />
+          </Link>
+        </div>
+        <div>
+          <h3>
+            <Link href="#" className=" line-clamp-2 truncate">
+              {title}
+            </Link>
+          </h3>
+          <div className="text-sm text-gray-400">
+            <div>
+              <Link href="#" className="text-pink-600">
+                {channelTitle}
+              </Link>
+            </div>
+            <div>
+              <span className="after:mx-1 after:content-['•']">
+                {viewCount} views
+              </span>
+              {/* <span>{data.videoAge}</span> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default VideoCard;
