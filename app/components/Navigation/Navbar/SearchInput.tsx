e61e49fa7f12329c58f6b6ed6004e5dc8bd77673
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-08-14 13:25:09 +0530

    feat(Search): Implemented Search Suggestions using custom API

--- app/components/Navigation/Navbar/SearchInput.tsx
+++ app/components/Navigation/Navbar/SearchInput.tsx
@@ -1,11 +1,34 @@
+/* eslint-disable prettier/prettier */
+
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai';
 import { ImMic } from 'react-icons/im';
+import { LuHistory } from 'react-icons/lu';
+import { YOUTUBE_SEARCH_API } from '@/utils/constants';
+import useFetchVideosData from '@/Hooks/useFetchVideosData';
 
 const SearchInput = () => {
   const [searchQuery, setSearchQuery] = useState('');
+
+  //* Debouncing
+
+  const { data, refetch } = useFetchVideosData(
+    'Search Suggestions',
+    `${YOUTUBE_SEARCH_API}${searchQuery}`,
+    false,
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => refetch(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery]);
+
   return (
     <div className="flex items-center justify-center gap-5">
       <form
@@ -13,23 +36,27 @@ const SearchInput = () => {
           e.preventDefault();
         }}
       >
-        <div className="flex h-10 items-center rounded-3xl border-2 border-zinc-900 bg-zinc-900 px-4 pr-0 hover:border-pink-600">
-          <div className="flex items-center gap-4 rounded-3xl pr-5">
+        <div
+          className={`flex h-10 items-center rounded-3xl ${
+            searchQuery && 'rounded-bl-none border-0 hover:border-none'
+          } border-2 border-zinc-900 bg-zinc-900 px-4 pr-0 hover:border-pink-600`}
+        >
+          <div className="flex items-center gap-4 rounded-3xl  pr-5">
             <div>
               <AiOutlineSearch className="text-xl" />
             </div>
             <input
-              type="text"
+              type=""
               className="peer h-[98%] w-96 border-none bg-zinc-900 focus:outline-none focus:ring-0"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
             />
 
             <AiOutlineClose
-            // className={`cursor-pointer rounded-3xl text-xl ${
-            //   !searchTerm ? 'invisible' : 'visible'
-            // }`}
-            // onClick={() => dispatch(clearSearchTerm())}
+              className={`cursor-pointer rounded-3xl text-xl ${
+                !searchQuery ? 'invisible' : 'visible'
+              }`}
+              onClick={() => setSearchQuery('')}
             />
           </div>
           <button
@@ -38,6 +65,18 @@ const SearchInput = () => {
           >
             <AiOutlineSearch className="text-2xl text-pink-600" />
           </button>
+        </div>
+        <div className="fixed w-[30.7rem] cursor-pointer rounded-b-xl border border-zinc-950 bg-zinc-950 bg-opacity-90 text-base shadow-lg backdrop-blur-3xl backdrop-filter">
+          <ul>
+            {data &&
+              data[1].map((suggestions: string) =>
+                suggestions === '' ? null : (
+                  <li className="my-2 flex gap-4 px-5 hover:bg-black hover:text-pink-600">
+                    <LuHistory className="mt-1 text-lg" /> {suggestions}
+                  </li>
+                ),
+              )}
+          </ul>
         </div>
       </form>
       <div className="rounded-full bg-zinc-900 p-3 text-xl ">
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,3 +2,10 @@ export const YOUTUBE_API_URL: string =
   'https://youtube.googleapis.com/youtube/v3';
 
 export const YOUTUBE_VIDEOS_API: string = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=15&key=${process.env.NEXT_PUBLIC_YOUTUBE_DATA_API_KEY}`;
+
+export const YOUTUBE_SEARCH_API: string =
+  'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+
+/* Official Youtube Search API
+  https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=
+  */
